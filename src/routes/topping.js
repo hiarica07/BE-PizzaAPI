bdde3edb8b93aa32c2fcf8f4584d4fb0fc5029ff
hiarica07 <halilarica@gmail.com>
2024-11-30 18:33:42 +0300

    "Added Topping controller, model, and routes with CRUD functionality"

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -4,6 +4,16 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const { list, create, read, update, deleteTopping } = require('../controllers/topping')
+// URL: /toppings
 
+router.route("/")
+.get(list)
+.post(create)
+router.route("/:id")
+.get(read).put(update)
+.patch(update)
+.delete(deleteTopping)
 /* ------------------------------------------------------- */
 module.exports = router
+
