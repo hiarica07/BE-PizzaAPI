e5792ff0284113ee8ecd4a5b0078eff184c7a444
hiarica07 <halilarica@gmail.com>
2024-11-30 15:49:02 +0300

    "Added TokenSchema and Token model to token.js file"

--- src/models/token.js
+++ src/models/token.js
@@ -4,3 +4,26 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const TokenSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+            index: true,
+        },
+
+        token: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            index: true,
+        },
+    },
+    { collection: "tokens", timestamps: true },
+);
+
+module.exports = mongoose.model("Token", TokenSchema);
