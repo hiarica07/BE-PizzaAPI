544201e38440c964170c4f04082a71151c67abea
hiarica07 <halilarica@gmail.com>
2024-11-30 16:57:30 +0300

    "Added Swagger annotations to login and logout functions in auth.js"

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,11 +7,33 @@ const User = require("../models/user")
 
 module.exports = {
     login: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "aA?123456",
+                }
+            }
+        */
+
         res.status(200).send({
             message: "Login Successfull"
         })
     },
     logout: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "simpleToken: Logout"
+            #swagger.description = 'Delete token key.'
+        */
+
         res.status(200).send({
             message: "Login Successfull"
         })
