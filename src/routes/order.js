fd00fae0b5debd5cef83dff0581120e88de69e6c
hiarica07 <halilarica@gmail.com>
2024-11-30 19:22:24 +0300

    "Added order controller, model, and routes with CRUD functionality"

--- src/routes/order.js
+++ src/routes/order.js
@@ -5,5 +5,13 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const { list, create, read, update, deleteOrder } = require('../controllers/order')
+
+// URL: /orders
+
+
+router.route('/').get(list).post(create)
+
+router.route('/:id').get(read).put(update).patch(update).delete(deleteOrder)
 /* ------------------------------------------------------- */
 module.exports = router
