9362b95e0bb49d9934adb752f98442ad639b980f
hiarica07 <halilarica@gmail.com>
2024-11-30 18:36:27 +0300

    "Added pizza controller, model, and routes with CRUD functionality"

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -5,5 +5,13 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const { list, create, read, update, deletePizza } = require('../controllers/pizza')
+
+// URL: /pizzas
+
+router.route('/').get(list).post(create)
+
+router.route('/:id').get(read).put(update).patch(update).delete(deletePizza)
+
 /* ------------------------------------------------------- */
 module.exports = router
