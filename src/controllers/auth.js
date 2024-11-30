75733a9aa9b3976b796f9a3fbb7c115cec689368
hiarica07 <halilarica@gmail.com>
2024-11-30 16:55:08 +0300

    "Added auth controller and routes for login and logout functionality"

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,3 +2,18 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const User = require("../models/user")
+
+module.exports = {
+    login: async (req, res) => {
+        res.status(200).send({
+            message: "Login Successfull"
+        })
+    },
+    logout: async (req, res) => {
+        res.status(200).send({
+            message: "Login Successfull"
+        })
+    },
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,11 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const router = require("express").Router()
+
+const {login, logout} =require("../controllers/auth")
+router.post("/login", login)
+router.post("/logout", logout)
+
 module.exports = router
